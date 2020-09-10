8e3d108328f780efae5e83bfc21997950124656e
Joonsey <59510166+Joonsey@users.noreply.github.com>
2020-09-10 10:05:03 +0200

    fix story

--- story.py
+++ story.py
@@ -5,7 +5,7 @@ class story:
         self.storylist = []
     
         for n in self.args:
-            if n in story_dict:
+            if n in self.StoryDict:
                 self.storylist.append(n)
             else:
                 pass
@@ -18,18 +18,24 @@ class story:
         def randomchoice(choices, ctx):
             return random.choice(choices.get(ctx))
 
-        StoryDict = {
+        self.StoryDict = {
         "mainMotive":["neutral good", "neutral evil", "neutral neutral", "chaotic good", "chaotic evil", "chaotic neutral", "Lawfull good", "lawfull evil", "lawfull neutral"],
         "location":["oslo", "spain", "london", "narnia", "azeroth", "pandora", "mars", "the moon"],
         "sideCharacter":["none","cat", "dog", "golem", "large rat"]
         }
-        self.storytheme = ", ".join([randomchoice(StoryDict, "mainMotive"), randomchoice(StoryDict, "location"), randomchoice(StoryDict, "sideCharacter")])
+        
+        self.storytheme = ["".join(randomchoice(self.StoryDict, "mainMotive")),
+        "".join(randomchoice(self.StoryDict, "location")),
+        "".join(randomchoice(self.StoryDict, "sideCharacter"))
+        #", ".join(randomchoice(self.StoryDict, "4th category"))
+        ]
         
         print(self.storytheme)
         
     def storyTheme(self):
         return self.storytheme
-
+s = story()
+s.embedStory()
 class story_loop:
     def __init__(self, outcome1, o2, o3):
         self.ANSWER_A = ["1","A","a"]
